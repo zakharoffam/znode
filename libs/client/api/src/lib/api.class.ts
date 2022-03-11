496d158a4d52499d126c5c3692744d5b354596aa
zakharoffam <zakharoff.am@ya.ru>
2022-03-12 00:13:24 +0400

    Снова добавлен модуль "Журнал событий".

---
+++ libs/client/api/src/lib/api.class.ts
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ClientApiProps {}
+
+export function ClientApi(props: ClientApiProps) {
+  return (
+    <div>
+      <h1>Welcome to ClientApi!</h1>
+    </div>
+  );
+}
+
+export default ClientApi;
